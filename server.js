e38ce91a183d0aa7610918c02c2c9192f27abb41
Loveneetahuja <loveneet.ahuja@yahoo.com>
2017-06-26 18:28:05 +0530

    heroku correct solution

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req,res,next){
-	if(req.protocol == 'http') {
-		next();
-		} else {
-			res.redirect('http://' + req.hostname + req.url);
+	 if (req.headers['x-forwarded-proto'] === 'https') { // WORKED
+      console.log("WR__ 00 HTTP *S* Inside IF : req.protocol : "  + req.protocol);
+      console.log("WR__ 01 HTTP *S* Inside IF : req.headers['x-forwarded-proto']"  + req.headers['x-forwarded-proto']);
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+			next();
 		}
 	});
 
