b6ca567ef2ec3799575249280af38057e6330c69
Loveneetahuja <loveneet.ahuja@yahoo.com>
2017-06-27 13:23:05 +0530

    About page modified

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,22 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About Component.</h3>
-      <p>Welcome to about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>
+      This shows Aaj ka mausam.
+
+      </p>
+      <p>
+      	Here are some tools I have used:
+      </p>
+      <ul>
+      	<li>
+      		<a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+      	</li>
+      	<li>
+      		<a href="http://openweathermap.org">Open Weather Map</a> - Open Weather to search for weather by location. 
+      	</li>
+      </ul>
     </div>
   )
 };
